b6db30370bfc09369207b45e7efb8eac9b4d1fe6
Federico Palomero Garcia <fedepalo13@gmail.com>
2018-01-23 10:04:51 -0300

    changes

--- app/src/main/java/com/example/federicopalomero/acamicaexample/MainActivity.java
+++ app/src/main/java/com/example/federicopalomero/acamicaexample/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/federicopalomero/acamicaexample/SecondActivity.java
+++ app/src/main/java/com/example/federicopalomero/acamicaexample/SecondActivity.java
@@ -3,6 +3,10 @@ package com.example.federicopalomero.acamicaexample;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by federico.palomero on 1/22/2018.
@@ -14,5 +18,15 @@ public class SecondActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
+
+        final EditText username= (EditText)findViewById(R.id.userText);
+        final EditText passwod=(EditText)findViewById(R.id.pwText);
+        final Button login=(Button)findViewById(R.id.btnLogIn);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SecondActivity.this, username.getText().toString() + passwod.getText().toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
