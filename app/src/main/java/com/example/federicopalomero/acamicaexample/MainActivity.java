575029c70f9e35a89d71c4ecdc7bcdb752657f00
Federico Palomero Garcia <fedepalo13@gmail.com>
2018-01-23 10:39:14 -0300

    added option bar

--- app/src/main/java/com/example/federicopalomero/acamicaexample/MainActivity.java
+++ app/src/main/java/com/example/federicopalomero/acamicaexample/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -56,5 +59,23 @@ public class MainActivity extends AppCompatActivity {
         Log.d( "onDestroy"," Entro al destroy");
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.main,menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.login:
+                Intent i= new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
 }
